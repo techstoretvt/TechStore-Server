c4790e498a59dec3004cfc0a36c76651c851a673
techstoretvt <techstoretvt2565@gmail.com>
2023-09-20 19:54:48 +0700

    update time token

--- src/helpers/JWT_service.js
+++ src/helpers/JWT_service.js
@@ -10,7 +10,7 @@ const signAccessToken = async (user_Id) => {
         };
         const secret = process.env.ACCESS_TOKEN_SECRET;
         const options = {
-            expiresIn: '10m',
+            expiresIn: '60m',
         };
 
         JWT.sign(payload, secret, options, (err, token) => {
