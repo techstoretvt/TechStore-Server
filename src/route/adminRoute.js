87e217b34142252353c4066f3afce9a240c4eaa9
techstoretvt <techstoretvt2565@gmail.com>
2023-10-11 14:28:01 +0700

    lan 2

--- src/route/adminRoute.js
+++ src/route/adminRoute.js
@@ -143,7 +143,6 @@ const initAdminRoute = (app) => {
     );
     router.get(
         routes.timKiemBaiHatById,
-        verifyAccessTokenAdmin,
         adminController.timKiemBaiHatById
     );
 
